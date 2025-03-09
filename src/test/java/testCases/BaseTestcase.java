de69eab0b5cbaf4fc43dbf07d89c3a0f9c3dee65
Ashik BR <ashikbr79@gmail.com>
2025-03-09 18:24:39 +0530

    created an xml file for cross browser testing and made changes to the setup method

--- src/test/java/testCases/BaseTestcase.java
+++ src/test/java/testCases/BaseTestcase.java
@@ -8,19 +8,31 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class BaseTestcase {
 	WebDriver driver;
 	Properties p;
 	@BeforeMethod
-	public void setup() throws IOException
+	@Parameters({"browser"})
+	public void setup(String br) throws IOException
 	{
 		FileInputStream file= new FileInputStream(".//src//test//resources//TestData.properties");
 		p=new Properties();
 		p.load(file);
-		driver=new ChromeDriver();
+		if(br.equalsIgnoreCase("chrome"))
+		{
+			driver=new ChromeDriver();
+			
+		}
+		
+		if(br.equalsIgnoreCase("edge"))
+		{
+			driver=new EdgeDriver();
+		}
 		driver.get(p.getProperty("appurl"));
 		driver.manage().window().maximize();
 	}
