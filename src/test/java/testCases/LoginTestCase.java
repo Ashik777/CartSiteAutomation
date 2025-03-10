2798b7ba87ddb6d5bdf7953deeaefa3fd4384aea
Ashik BR <ashikbr79@gmail.com>
2025-03-10 19:24:14 +0530

    added log4j file to project and made changes in the base class

--- src/test/java/testCases/LoginTestCase.java
+++ src/test/java/testCases/LoginTestCase.java
@@ -15,9 +15,12 @@ public class LoginTestCase extends BaseTestcase {
 		LoginPage login = new LoginPage(driver);
 		login.enterUsername(p.getProperty("username"));
 		login.enterPassword(p.getProperty("password"));
+		logger.info("enterd user name and password");
 		login.clickLoginButton();
+		logger.info("clicked on the loggin button");
 		MyAccountPage acpage= new MyAccountPage(driver);
 		boolean status1= acpage.checksignoutbutton();
+		logger.info("clicked on signout button");
 		Assert.assertEquals(status1, true);
 		acpage.clickSignout();
 		
